c6c9a569973e0242b70189aa63541f4d5dc7f03d
sirily11 <sirily1997@gmail.com>
2021-08-27 15:26:58 +0800

    update design

--- js/blockInfo.js
+++ js/blockInfo.js
@@ -2,8 +2,30 @@
 
 console.log("init");
 const socket = io("wss://etd.monitor.sirileepage.com/clients");
+const data = {
+  labels: ["a", "b"],
+  datasets: [
+    {
+      label: "My First dataset",
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "rgb(255, 99, 132)",
+      data: [0, 10, 5, 2, 20, 30, 45],
+    },
+  ],
+};
+
+const config = {
+  type: "line",
+  data: data,
+  options: {},
+};
 
 socket.on("history", (data) => {
+  const block_time_history_display =
+    document.querySelector("#blocktime-history");
+
+  const difficulty_display = document.querySelector("#difficulty-history");
+
   const block_time_display = document.querySelector("#num_block_time");
   const block_number_display = document.querySelector("#num_blocks");
   const network_hashrate_display = document.querySelector("#num_hash_rate");
